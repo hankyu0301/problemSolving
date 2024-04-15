5f36ff47f29a78c98dd7c170a9da79a73e6b2936
hankyujang <finebears@naver.com>
2024-04-15 18:17:35 +0900

    240415

---
+++ src/programmers/문자열/문자열_다루기_기본.java
@@ -0,0 +1,33 @@
+package programmers.문자열;
+
+public class 문자열_다루기_기본 {
+
+    /*문제 설명
+    문자열 s의 길이가 4 혹은 6이고, 숫자로만 구성돼있는지 확인해주는 함수, solution을 완성하세요.
+    예를 들어 s가 "a234"이면 False를 리턴하고 "1234"라면 True를 리턴하면 됩니다.
+
+    제한 사항
+    s는 길이 1 이상, 길이 8 이하인 문자열입니다.
+    s는 영문 알파벳 대소문자 또는 0부터 9까지 숫자로 이루어져 있습니다.
+    */
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean solution(String s) {
+        char[] chars = s.toCharArray();
+
+        for( char ch : chars ) {
+            if(Character.isAlphabetic(ch)) {
+                return false;
+            }
+        }
+
+        if (!(chars.length == 4 || chars.length == 6)) {
+            return false;
+        }
+
+        return true;
+    }
+}
