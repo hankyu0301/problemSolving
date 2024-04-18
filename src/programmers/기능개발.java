2e84eb68b8c63143cc12acb4f237882988152bde
hankyujang <finebears@naver.com>
2024-04-18 20:33:42 +0900

    240418

---
+++ src/programmers/기능개발.java
@@ -0,0 +1,38 @@
+package programmers;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class 기능개발 {
+
+    class Solution {
+        public int[] solution(int[] progresses, int[] speeds) {
+            Queue<Integer> q = new LinkedList<>();
+            List<Integer> answerList = new ArrayList<>();
+
+            for (int i = 0; i < speeds.length; i++) {
+                double remain = (100 - progresses[i]) / (double) speeds[i];
+                int date = (int) Math.ceil(remain);
+
+                if (!q.isEmpty() && q.peek() < date) {
+                    answerList.add(q.size());
+                    q.clear();
+                }
+
+                q.offer(date);
+            }
+
+            answerList.add(q.size());
+
+            int[] answer = new int[answerList.size()];
+
+            for (int i = 0; i < answer.length; i++) {
+                answer[i] = answerList.get(i);
+            }
+
+            return answer;
+        }
+    }
+}
